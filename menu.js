2f32ccc1af3523a397e438b6cc002c18868264f4
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2023-02-11 07:57:41 +0000

    Add files via upload
    Uploading source file.

---
+++ menu.js
@@ -0,0 +1,16 @@
+//1
+let item1 = "Ice cream";
+let item2 = "Fish";
+let item3 = "Chips";
+
+//2
+console.log(`Order Confirmed:
+1. ${item1}
+2. ${item2}
+3. ${item3}`);
+
+/*
+    Thinking
+        1. Created three variables containing 3 items which I like.
+        2. These are printed out to the console. I've used a template literal here to get the necessary spacing bewteen the items.
+*/
